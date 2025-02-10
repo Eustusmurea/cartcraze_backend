6faefa6cec25214ef5a07ab56ea3d7f47a7c781b
Eustus Mwirigi <mwirigieustus97@gmail.com>
2025-02-10 22:35:38 +0300

    refactor: update settings to use hardcoded SECRET_KEY and comment out sensitive information in .env file

--- backend/settings.py
+++ backend/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import Config, Csv
-config = Config()
-print(config('SECRET_KEY'))
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%g)h65x9pvvqk6!bn9f#dt3^owry@^@w=$$jt8t*=$^zgj^&x%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
