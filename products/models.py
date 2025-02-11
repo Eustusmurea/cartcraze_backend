cba956edfb887a15725dee5900656c44d866eb2a
Eustus Mwirigi <mwirigieustus97@gmail.com>
2025-02-11 18:56:46 +0300

    feat: add product and order models with migrations; update admin registration and image upload path

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock=models.IntegerField(default=0)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to='products/images/', null=True, blank=True)
 
 def __str__(self):
     return self.name
