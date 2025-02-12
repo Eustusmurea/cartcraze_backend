f9d685e578a519a777ed675b5648465698ca89eb
Eustus Mwirigi <mwirigieustus97@gmail.com>
2025-02-12 08:37:01 +0300

    Update media folder in settings

--- backend/settings.py
+++ backend/settings.py
@@ -145,6 +145,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
