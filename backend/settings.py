6ef359a9bee9759de37912908d38fc6aecd33e88
Eustus Mwirigi <mwirigieustus97@gmail.com>
2025-02-17 11:52:46 +0300

    feat: enable CORS credentials support and add CorsMiddleware to settings

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,7 @@ STRIPE = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ALLOWED_HOSTS = []
 
@@ -69,6 +70,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
